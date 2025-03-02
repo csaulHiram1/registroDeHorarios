088762f0d21e45201e7a429c15cddfdfe02833d9
Saul Hiram Castillo Castro <csaulhiram@gmail.com>
2025-03-02 17:22:26 -0600

    preparing db.php to production

--- db.php
+++ db.php
@@ -1,10 +1,31 @@
 <?php
-$host = "localhost"; // Servidor de la base de datos
-$dbname = "registrarhorarios"; // Nombre de la base de datos
-$username = "root"; // Usuario de la base de datos
-$password = ""; // Contraseña de la base de datos
+$host = getenv('MYSQLHOST');
+$dbname = getenv('MYSQLDATABASE');
+$username = getenv('MYSQLUSER');
+$password = getenv('MYSQLPASSWORD');
+$port = getenv('MYSQLPORT') ?: 3306; // Puerto por defecto
 
 try {
+    // Crear una nueva conexión PDO con los datos de Railway
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $username, $password);
+
+    // Configurar el modo de errores de PDO para lanzar excepciones
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    echo "✅ Conexión exitosa a la base de datos.";
+} catch (PDOException $e) {
+    echo "❌ Error de conexión: " . $e->getMessage();
+}
+?>
+
+
+
+
+
+
+
+
+/* try {
     // Crear una nueva conexión PDO
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
 
@@ -15,4 +36,4 @@ try {
 } catch (PDOException $e) {
     echo "❌ Error de conexión: " . $e->getMessage();
 }
-
+ */
